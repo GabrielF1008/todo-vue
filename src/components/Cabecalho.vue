95d15f952492da46fca2bfca9923d2dedf591399
Gabriel Oehlerick <gabrielfelipe1009@gmail.com>
2024-07-08 17:12:49 -0300

    duvida

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const props = defineProps(['getTarefasPendentes'])
+</script>
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ props.getTarefasPendentes().length }} tarefas pendentes
+        </p>
+    </header>
+</template>
